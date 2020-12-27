170c76294a1c7d9bab6ddfa7dfb19105574ffd4f
kaly <kalimamadou@gmail.com>
2020-12-27 12:39:06 -0800

    Using SQLAlchemy to read data from todoapp database and display on the webapp

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Jerico05@localhost:5433/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description= db.Column(db.String(), nullable=False)
+
+    def __repr(self):
+        return f'<Todo {self.id} {self.description}'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[
-        {'description': 'todo 1'},
-        {'description': 'todo 2'},
-        {'description': 'todo 3'},
-        {'description': 'todo 4'},
-    ])
+    return render_template('index.html', data=Todo.query.all())
 
 
 if __name__ == "__main__":
